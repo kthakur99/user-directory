adcbe06ef8ccac960082b39ca641b63829257a02
Kunal Thakur <KunalThakur@Kunals-MacBook-Pro-2.local>
2018-05-14 17:22:44 -0400

    Changes the text of the button

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('It works!')
+const button = document.querySelector('button.greeting')
+//const changetxt
+
+function changeText() {
+    // get the element you want to change the text of.
+    //button
+    // get the text you want to change the element to.
+    const p = document.querySelector('p.greeting');
+    // FINAL STEP: change the text.
+    button.textContent = p.textContent;
+}
+
+
+button.addEventListener('click', changeText)
+
+
