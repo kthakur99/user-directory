fe3b6be0d8f39f0e863f4e44f8af04c06d89673b
Kunal Thakur <KunalThakur@kunals-mbp-2.coldspring.marian.edu>
2018-05-15 19:55:19 -0400

    change the text of the second heading

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
 const button = document.querySelector('button.greeting')
-const h1 = document.querySelector('h1')
 
-function changeText() {
-    
-    const p = document.querySelector('p.greeting');
-    const txt = document.querySelector("input#txt")
-    h1.textContent = p.textContent;
+
+function changeText(ev) {
+    const heading = document.querySelector('.changeThisOne');
+    heading.textContent = 'Clickity click'
 }
 
 button.addEventListener('click', changeText)
-
-const form = document.querySelector('#userForm')
-const handleSubmit = function(ev)
-{
-    ev.preventDefault()
-    const users = document.querySelector('#users')
-    const f = ev.target
-    users.innerHTML += '<p>' + userName + '</p>'
-    
-
-    f.username.value = ''
-
-}
-FormData.addEventListener('submit', handleSubmit)
