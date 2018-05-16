4c04dfb9cddbf57c93cbae0beabecb39db1086c8
Kunal Thakur <KunalThakur@kunals-mbp-2.coldspring.marian.edu>
2018-05-16 11:11:50 -0400

    Display users in an unordered list

--- index.js
+++ index.js
@@ -8,12 +8,20 @@ const handleSubmit = function(ev)
     const userName = f.userName.value
     const age = f.age.value
     const favoriteColor = f.favoriteColor.value
+   
+    const nameItem = document.createElement('li')
+    nameItem.textContent = `Name: ${userName}`
 
-    const p = document.createElement('p')
-    p.textContent = `${userName}, ${age}`
+    const ageItem = document.createElement('li')
+    ageItem.textContent = `Age: ${age}`
+
     
-    p.style.backgroundColor = favoriteColor
-    users.appendChild(p)
+    const list = document.createElement('ul')
+    list.appendChild(nameItem)
+    list.appendChild(ageItem)
+
+    const users = document.querySelector('#users')
+    users.appendChild(list)
 
     f.reset()
     f.userName.focus()
