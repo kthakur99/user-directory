e2e3e4df75fa5e3ab346d6a99a5a641c12420597
Kunal Thakur <KunalThakur@kunals-mbp-2.coldspring.marian.edu>
2018-05-16 11:51:24 -0400

    Display color in empty div

--- index.js
+++ index.js
@@ -14,13 +14,22 @@ const handleSubmit = function(ev)
 
     const ageItem = document.createElement('li')
     ageItem.textContent = `Age: ${age}`
-
     
+    const colorItem = document.createElement('li')
+    colorItem.textContent = 'Favorite Color: '
+   
+    const colorDiv = document.createElement('div')
+    colorDiv.style.backgroundColor = favoriteColor
+    colorDiv.style.width = '6rem'
+    colorDiv.style.height = '3rem'
+
+    colorItem.appendChild(colorDiv)
+
     const list = document.createElement('ul')
     list.appendChild(nameItem)
     list.appendChild(ageItem)
-
-    const users = document.querySelector('#users')
+    list.appendChild(colorItem)
+    
     users.appendChild(list)
 
     f.reset()
