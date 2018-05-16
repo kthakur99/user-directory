c1fd2bb33feebface0c3ec131132ef5b253a2c8c
Kunal Thakur <KunalThakur@kunals-mbp-2.coldspring.marian.edu>
2018-05-16 05:48:29 -0400

    Add Age Field

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const handleSubmit = function(ev)
     const users = document.querySelector('#users')
     const f = ev.target
     const userName = f.userName.value
+    const age = f.age.value
     users.textContent += ' ' + userName 
-    user.innerHTML += '<p>' + userName + </p>
+    users.innerHTML += '<p>' + userName + ', ' + age + '</p>'
+    
     f.userName.value = ' '
 }
 
